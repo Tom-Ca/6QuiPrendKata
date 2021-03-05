62dc826bdaef01db049dd287e9f8ede3913f5e1e
Tom Careghi <tom.careghi@gmail.com>
2021-03-05 20:16:08 +0100

    ud

--- java/SixQuiPrend.java
+++ java/SixQuiPrend.java
@@ -1,18 +1,39 @@
-
-
 public class SixQuiPrend {
+
+    public static int cardValue;
+    public static int multiple5 = 5;
+    public static int multiple10 = 10;
+    public static int multiple11= 11;
+    public static int nbbeef;
+
 
     private static final int NB_CARD_MAX = 104;
 
     public static int findNumberOfBullHeads(int value) {
-        // TODO
+        nbbeef = 0;
+        if (cardValue % multiple5 == 0) {
+            nbbeef = 2;
+        }
+        if (cardValue % multiple10 == 0) {
+            nbbeef = 3;
+        }
+        if (cardValue %  multiple11 == 0 && cardValue != 100){
+            nbbeef = 5;
+        }
+        if (cardValue == 55 ){
+            nbbeef += 2;
+        }
+        if (nbbeef ==0){
+            nbbeef=1;
+        }
 
-        return 0;
+        return nbbeef;
     }
 
     public static void gameLoop() {
-        for (int cardValue = 1 ; cardValue <= NB_CARD_MAX ; cardValue++) {
+        for (cardValue =1; cardValue <= NB_CARD_MAX ; cardValue++) {
             System.out.println(cardValue + " -> " + findNumberOfBullHeads(cardValue));
         }
+
     }
 }
